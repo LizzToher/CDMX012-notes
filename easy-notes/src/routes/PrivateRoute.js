c783cf5043313a708d9c0fc832d669f4ce247e3e
Allison Tovar <alis.th94@gmail.com>
2022-05-05 14:10:39 -0500

    Este commit establece las rutas públicas y privadas

---
+++ easy-notes/src/routes/PrivateRoute.js
@@ -0,0 +1,23 @@
+import { auth } from "../firebase/firebase-auth";
+import Header from "../components/Header";
+import logoutIcon from "../assets/logout.png";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../views/Home";
+import { AddNote } from "../views/AddNote";
+
+const PrivateRoute = ({ logoutBtn }) => {
+  const user = auth.currentUser;
+  return (
+    <>
+      <img src={logoutIcon} className="logout-btn" alt="Cerrar sesión" onClick={logoutBtn}></img>
+      <Header input={user.displayName} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddNote />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+};
+
+export default PrivateRoute;
---
+++ easy-notes/src/routes/PublicRoute.js
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
+import { logInWithGoogle } from "../firebase/firebase-auth";
+import Login from "../views/Login";
+import Register from "../views/Register";
+
+const PublicRoute = () => {
+    return(
+        <>
+        <Header input='Easy Notes!' />
+        <Routes>
+            <Route path="/" element={<Login loginBtn={logInWithGoogle} />} />
+            <Route path="/register" element={<Register loginBtn={logInWithGoogle} />} />
+            <Route path="*" element={<Navigate to="/register" replace />} />
+        </Routes>
+        </>
+    )
+}
+
+export default PublicRoute;
