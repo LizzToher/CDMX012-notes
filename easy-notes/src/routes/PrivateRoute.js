9337668b0a1b2472155d6755b12ea03aa13b0c6b
Allison Tovar <alis.th94@gmail.com>
2022-05-10 21:46:13 -0500

    Esto hizo la ruta de cada nota para visualizarla, editarla y eliminarla

--- easy-notes/src/routes/PrivateRoute.js
+++ easy-notes/src/routes/PrivateRoute.js
@@ -3,6 +3,7 @@ import logoutIcon from "../assets/logout.png";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import { AddNote } from "../views/AddNote";
+import Note from "../views/Note";
 
 const PrivateRoute = ({ user, logoutBtn }) => {
   return (
@@ -11,6 +12,7 @@ const PrivateRoute = ({ user, logoutBtn }) => {
       <Header input={user.displayName} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Note />} />
         <Route path="/add" element={<AddNote />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
