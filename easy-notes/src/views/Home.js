4c7624427055974081e488f52ede541a4b16cb9c
Allison Tovar <alis.th94@gmail.com>
2022-05-10 21:43:43 -0500

    Esto hizo cambios menores en la ruta de la página

--- easy-notes/src/views/Home.js
+++ easy-notes/src/views/Home.js
@@ -1,21 +1,26 @@
 import { useNavigate } from "react-router-dom";
 import addBtn from "../assets/add-note.png";
-import Note from "../components/ViewNotes";
+import ViewNotes from "../components/ViewNotes";
 
 const Home = () => {
   const navigate = useNavigate();
 
   const handleOnClick = () => {
-    navigate('/add');
-  }
+    navigate("/add");
+  };
 
   return (
-      <main className="dashboard">
-        <h2>Mis notas</h2>
-        <img src={addBtn} className="add-btn" alt="Agregar nota" onClick={handleOnClick}></img>
-        <Note />
-      </main>
+    <main className="dashboard">
+      <h2>Mis notas</h2>
+      <img
+        src={addBtn}
+        className="add-btn"
+        alt="Agregar nota"
+        onClick={handleOnClick}
+      ></img>
+      <ViewNotes />
+    </main>
   );
-}
+};
 
 export default Home;
